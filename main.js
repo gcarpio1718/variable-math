c927d10f38359c01d2b74c7684907c686df26753
gcarpio1718 <gcarpio1221@gmail.com>
2022-02-07 14:11:44 -0500

    change main

---
+++ main.js
@@ -0,0 +1,23 @@
+let x = 32;
+let y = 5;
+let sum = x+y;
+console.log("The sum of my age plus five years equals: " + "37");
+
+console.log("\n");
+
+const num1 = 5;
+const num2 = 3;
+let total = num1 + num2;
+console.log("The total of num1 and num2 is: " + total);
+
+console.log("\n");
+
+var name = "Giovanni Carpio";
+console.log ("They call me: " + name);
+
+console.log("\n");
+
+let a = 4;
+let b = 7;
+let dividend = b%a;
+console.log("the remainder of 7 and 4 is: " + " 3");
---
+++ main1.js
@@ -0,0 +1,29 @@
+const prompt = require('prompt-sync')();
+
+//This is a javascript comment!
+
+/*
+This
+is
+a
+multiline
+comment
+*/
+
+//console.log("Good morning!");
+
+// console.log (Math.sqrt(144));
+// console.log(Math.pow(12,2));
+// console.log(12 ** 2); 
+
+// let a = 5;
+// let b = 10;
+
+//a += b 
+
+let num = Number (prompt("Enter a number: "));
+//let num2 =7;
+
+console.log("Your variable is of type: " + num);
+
+//console.log("Your variable is of type: " + typeof num);
